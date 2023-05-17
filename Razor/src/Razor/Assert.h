c74a2a4305dd8e58b1a78b9062c78f63fbba5aa6
skyler.riggle <Skyler.D.Riggle-1@ou.edu>
2023-05-17 15:34:34 -0500

    Basic Loggin & Warning Messages

--- Razor/src/Razor/Assert.h
+++ Razor/src/Razor/Assert.h
@@ -7,10 +7,15 @@
 	#define ASSERT(expr) \
 			if (!(expr)) \
 			{ \
-				std::cerr << "Assertion Failed: `" << #expr << '`' << std::endl << \
-				"Error occured at line " << __LINE__ << " in file " << __FILE__ << std::endl; \
+				std::cerr << "\033[1;31m" << "ERROR: `" << #expr << '`' << std::endl << \
+				"Error occured at line " << __LINE__ << " in file " << __FILE__ << "\033[0m" \
+				<< std::endl; \
 				DEBUG_BREAK(); \
 			}
+	#define LOG(expr) std::cout << "LOG: " << expr << std::endl;
+	#define WARN(expr) std::cout << "\033[1;33m" << "WARN: " << expr << "\033[0m" << std::endl;
 #else
 	#define ASSERT(expr)
+	#define LOG(expr)
+	#define WARN(expr)
 #endif // RZ_DEBUG_BUILD
